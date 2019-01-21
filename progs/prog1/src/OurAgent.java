4e9359216c5cd2b72059487bfce5dd0751e26ddc
Pedro Hrafn Martinez <pedro16@ru.is>
2019-01-21 16:19:04 +0000

    exexute move added to State class and reading percepts in the init function in OurAgent should be ready

--- progs/prog1/src/OurAgent.java
+++ progs/prog1/src/OurAgent.java
@@ -1,48 +1,95 @@
 import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class OurAgent implements Agent
-{
+public class OurAgent implements Agent {
 	private Random random = new Random();
 
 	/*
-		init(Collection<String> percepts) is called once before you have to select the first action. Use it to find a plan. Store the plan and just execute it step by step in nextAction.
-	*/
+	 * init(Collection<String> percepts) is called once before you have to select
+	 * the first action. Use it to find a plan. Store the plan and just execute it
+	 * step by step in nextAction.
+	 */
 
-    public void init(Collection<String> percepts) {
+	public void init(Collection<String> percepts) {
 		/*
-			Possible percepts are:
-			- "(SIZE x y)" denoting the size of the environment, where x,y are integers
-			- "(HOME x y)" with x,y >= 1 denoting the initial position of the robot
-			- "(ORIENTATION o)" with o in {"NORTH", "SOUTH", "EAST", "WEST"} denoting the initial orientation of the robot
-			- "(AT o x y)" with o being "DIRT" or "OBSTACLE" denoting the position of a dirt or an obstacle
-			Moving north increases the y coordinate and moving east increases the x coordinate of the robots position.
-			The robot is turned off initially, so don't forget to turn it on.
-		*/
+		 * Possible percepts are: - "(SIZE x y)" denoting the size of the environment,
+		 * where x,y are integers - "(HOME x y)" with x,y >= 1 denoting the initial
+		 * position of the robot - "(ORIENTATION o)" with o in {"NORTH", "SOUTH",
+		 * "EAST", "WEST"} denoting the initial orientation of the robot - "(AT o x y)"
+		 * with o being "DIRT" or "OBSTACLE" denoting the position of a dirt or an
+		 * obstacle Moving north increases the y coordinate and moving east increases
+		 * the x coordinate of the robots position. The robot is turned off initially,
+		 * so don't forget to turn it on.
+		 */
 		Pattern perceptNamePattern = Pattern.compile("\\(\\s*([^\\s]+).*");
-		for (String percept:percepts) {
+		int posX, posY, sizeX, sizeY;
+		posX = posY = sizeX = sizeY = 0;
+		String orientation = "";
+		List<Integer> dirtX = new ArrayList<Integer>();
+		List<Integer> dirtY = new ArrayList<Integer>();
+		List<Integer> blockX = new ArrayList<Integer>();
+		List<Integer> blockY = new ArrayList<Integer>();
+		for (String percept : percepts) {
+			System.out.println("current percept:" + percept);
 			Matcher perceptNameMatcher = perceptNamePattern.matcher(percept);
 			if (perceptNameMatcher.matches()) {
 				String perceptName = perceptNameMatcher.group(1);
 				if (perceptName.equals("HOME")) {
 					Matcher m = Pattern.compile("\\(\\s*HOME\\s+([0-9]+)\\s+([0-9]+)\\s*\\)").matcher(percept);
 					if (m.matches()) {
-						System.out.println("robot is at " + m.group(1) + "," + m.group(2));
+						posX = Integer.parseInt(m.group(1));
+						posY = Integer.parseInt(m.group(2));
+						// System.out.println("robot is at " + m.group(1) + "," + m.group(2));
 					}
+				} else if (perceptName.equals("SIZE")) {
+					Matcher m = Pattern.compile("\\(\\s*SIZE\\s+([0-9]+)\\s+([0-9]+)\\s*\\)").matcher(percept);
+					if (m.matches()) {
+						sizeX = Integer.parseInt(m.group(1));
+						sizeY = Integer.parseInt(m.group(2));
+						// System.out.println("size of grid is " + m.group(1) + "," + m.group(2));
+					}
+				} else if (perceptName.equals("AT")) {
+					Matcher m = Pattern.compile("\\(\\s*AT\\s+((DIRT)|(OBSTACLE))\\s+([0-9]+)\\s+([0-9]+)\\s*\\)")
+							.matcher(percept);
+					if (m.matches()) {
+						if (m.group(1).equals("DIRT")) {
+							dirtX.add(Integer.parseInt(m.group(4)));
+							dirtY.add(Integer.parseInt(m.group(5)));
+							// System.out.println("dirt at " + m.group(4) + "," + m.group(5));
+						} else {
+							blockX.add(Integer.parseInt(m.group(4)));
+							blockY.add(Integer.parseInt(m.group(5)));
+							// System.out.println("obstacle at " + m.group(4) + "," + m.group(5));
+						}
+					}
+				} else if (perceptName.equals("ORIENTATION")) {
+					orientation = percept.substring(percept.indexOf(' ') + 1, percept.indexOf(')'));
+					// System.out.println("Orientation: " + orientation);
 				} else {
-					System.out.println("other percept:" + percept);
+					System.out.println("other percept: " + percept);
 				}
 			} else {
 				System.err.println("strange percept that does not match pattern: " + percept);
 			}
 		}
-    }
+		System.out.println("current position: " + posX + ", " + posY);
+		System.out.println("orientation: " + orientation);
+		System.out.println("size of grid: " + sizeX + ", " + sizeY);
+		for (int i = 0; i < dirtX.size(); i++) {
+			System.out.println("dirt at: " + dirtX.get(i) + ", " + dirtY.get(i));
+		}
+		for (int i = 0; i < blockX.size(); i++) {
+			System.out.println("obstacle at: " + blockX.get(i) + ", " + blockY.get(i));
+		}
+	}
 
-    public String nextAction(Collection<String> percepts) {
+	public String nextAction(Collection<String> percepts) {
 		System.out.print("perceiving:");
-		for(String percept:percepts) {
+		for (String percept : percepts) {
 			System.out.print("'" + percept + "', ");
 		}
 		System.out.println("");
--- progs/prog1/src/State.java
+++ progs/prog1/src/State.java
@@ -1,15 +1,18 @@
 import java.util.Collection;
 import java.util.ArrayList;
 
-public class State
-{
+public class State {
     public int posX, posY;
     public boolean on;
     public String orientation;
-    public int dirtsCleaned; 
+    public int dirtsCleaned;
 
-    public Collection<String> availableMoves() {
+    public Collection<String> availableMoves(int sizeX, int sizeY) {
         Collection<String> moves = new ArrayList<String>();
+        if (!on) {
+            moves.add("TURN_ON");
+            return moves;
+        }
         moves.add("GO");
         return moves;
     }
@@ -21,8 +24,43 @@ public class State
         tmp.on = this.on;
         tmp.orientation = this.orientation;
         tmp.dirtsCleaned = this.dirtsCleaned;
-
-
+        if (move.equals("TURN_ON")) {
+            tmp.on = true;
+        } else if (move.equals("GO")) {
+            if (orientation.equals("NORTH")) {
+                tmp.posY++;
+            } else if (orientation.equals("EAST")) {
+                tmp.posX++;
+            } else if (orientation.equals("SOUTH")) {
+                tmp.posY--;
+            } else {
+                tmp.posX--;
+            }
+        } else if (move.equals("TURN_LEFT")) {
+            if (orientation.equals("NORTH")) {
+                tmp.orientation = "WEST";
+            } else if (orientation.equals("EAST")) {
+                tmp.orientation = "NORTH";
+            } else if (orientation.equals("SOUTH")) {
+                tmp.orientation = "EAST";
+            } else {
+                tmp.orientation = "SOUTH";
+            }
+        } else if (move.equals("TURN_RIGHT")) {
+            if (orientation.equals("NORTH")) {
+                tmp.orientation = "EAST";
+            } else if (orientation.equals("EAST")) {
+                tmp.orientation = "SOUTH";
+            } else if (orientation.equals("SOUTH")) {
+                tmp.orientation = "WEST";
+            } else {
+                tmp.orientation = "NORTH";
+            }
+        } else if (move.equals("SUCK")) {
+            dirtsCleaned++;
+        } else {
+            tmp.on = false;
+        }
         return tmp;
     }
 }
