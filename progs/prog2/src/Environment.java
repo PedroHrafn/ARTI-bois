26c9286db7a428dea2fbcedbae4b0426571f9fe4
Johanne <johannegi16@ru.is>
2019-02-12 15:31:09 +0000

    env

--- progs/prog2/src/Environment.java
+++ progs/prog2/src/Environment.java
@@ -1,6 +1,14 @@
 public class Environment {
 
-    public Environment() {
+    public int width, height;
+    String role; 
+    int playclock;
+
+    public Environment(String role, int width, int height, int playclock) {
+        this.role = role;
+        this.width = width;
+        this.height = height;
+        this.playclock = playclock;
         
     }
 
