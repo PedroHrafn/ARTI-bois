ab4478ad0aca148cd819bca295baae8710084e09
Pedro Hrafn Martinez <pedro16@ru.is>
2019-02-12 16:48:16 +0000

    added todo for available moves in state

--- progs/prog2/src/State.java
+++ progs/prog2/src/State.java
@@ -1,5 +1,8 @@
 import java.util.Collection;
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import javafx.util.Pair;
+
 import java.awt.Point;
 
 public class State {
@@ -13,9 +16,20 @@ public class State {
         this.blacks = new CopyOnWriteArrayList<Point>(blacks);
     }
 
-    public Collection<String> availableMoves(int sizeX, int sizeY) {
+    public Collection<Pair<Point, Point>> availableMoves(boolean whiteTurn) {
         Collection<String> moves = new ArrayList<String>();
+        if (whiteTurn) {
+            for (Point from : whites) {
+                // TODO: check if from.y + 1 is empty
 
+                if (from.x != 1) {
+                    // check if there is black at (from.x - 1, from.y + 1)
+                }
+                if (from.x != env.width) {
+                    // check if there is black at (from.x - 1)
+                }
+            }
+        }
         return moves;
     }
 
