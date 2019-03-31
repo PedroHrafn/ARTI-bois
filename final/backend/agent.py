57eb07af7fe2fb666bdbd7083623843253c669b0
Pedro Hrafn Martinez <pedro16@ru.is>
2019-03-31 20:46:36 +0000

    made an evaluation function, fixed check if the game is draw or loss in absearch

--- final/backend/agent.py
+++ final/backend/agent.py
@@ -4,18 +4,19 @@ LOSS = -100
 WIN = 100
 DRAW = 25
 
+
 class Agent(object):
     def __init__(self, playclock, state):
         self.start = time.time()  # Used for stopping the search when the time has run out
         self.symbol = "O"
-        self.last_move = [] #TODO: check if this is correctly initialized
+        self.last_move = []  # TODO: check if this is correctly initialized
         self.won = []
         self.playclock = playclock
         self.state = state
-    
+
     def nextAction(self):
         self.start = time.time()
-        h = 1 #TODO: find better starting value for depth
+        h = 1  # TODO: find better starting value for depth
         move = []
         value = float("-inf")
         try:
@@ -26,22 +27,22 @@ class Agent(object):
             print(f"Stopped at height: {h}")
 
         return move
-    
+
     def abSearchRoot(self, h):
         alpha = float("-inf")
         beta = float("inf")
         max_value = float("-inf")
-        best_move = [] 
+        best_move = []
 
         for move in self.state.availableMoves():
-            value = self.abSearch(move, self.last_move,alpha, beta, h, True)
+            value = self.abSearch(move, self.last_move, alpha, beta, h, True)
             if value > max_value:
                 max_value = value
                 alpha = value
                 best_move = move
 
         return max_value, best_move
-    
+
     def abSearch(self, move, prev_next_big, alpha, beta, h, maximize):
         # Check if time has ran out
         if time.time() - self.start > self.playclock:
@@ -50,14 +51,14 @@ class Agent(object):
         # Check if we have reached a terminal state
         if self.state.won == self.symbol:
             return WIN
-        elif self.state.won != "D" and self.state.won != self.symbol:
-            return LOSS
-        elif self.state.won:
+        elif self.state.won == "D":
             return DRAW
+        elif self.state.won:
+            return LOSS
 
         # Check if max depth is reached
         if h == 0:
-            # took in laststate and 
+            # took in laststate and
             return self.evaluateState(self.state)
 
         # Apply alpha beta pruning
@@ -66,7 +67,8 @@ class Agent(object):
             # Get next state, state changes when calling makeMove
             prev_next_big = self.state.next_big
             self.state.makeMove(move)
-            v = self.abSearch(move, self.state.next_big, alpha, beta, h - 1, not maximize)
+            v = self.abSearch(move, self.state.next_big,
+                              alpha, beta, h - 1, not maximize)
             if maximize:
                 if v > best_value:
                     best_value = v
@@ -85,6 +87,11 @@ class Agent(object):
         return best_value
 
     def evaluateState(self, state):
-        #TODO: find heuristic
-        return 25
-    
+        score = 0
+        for big_row in state.big_board:
+            for small_board in big_row:
+                if small_board["status"] == self.symbol:
+                    score += 10
+                elif small_board["status"] != '' and small_board["status"] != 'D':
+                    score -= 10
+        return score
