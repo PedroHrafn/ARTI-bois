1b370dd6df488b87af9f2e94186d58b2a920925b
Alex Már Gunnarsson <alex16@ru.is>
2019-03-31 20:22:57 +0000

    added state to the variables of the agent

--- final/backend/agent.py
+++ final/backend/agent.py
@@ -5,35 +5,36 @@ WIN = 100
 DRAW = 25
 
 class Agent(object):
-    def __init__(self, playclock):
+    def __init__(self, playclock, state):
         self.start = time.time()  # Used for stopping the search when the time has run out
         self.symbol = "O"
         self.last_move = [] #TODO: check if this is correctly initialized
         self.won = []
         self.playclock = playclock
+        self.state = state
     
-    def nextAction(self, state):
+    def nextAction(self):
         self.start = time.time()
         h = 1 #TODO: find better starting value for depth
         move = []
         value = float("-inf")
         try:
             while value != 100:
-                value, move = self.abSearchRoot(h, state)
+                value, move = self.abSearchRoot(h, self.state)
                 h += 1
         except:
             print(f"Stopped at height: {h}")
 
         return move
     
-    def abSearchRoot(self, h, state):
+    def abSearchRoot(self, h):
         alpha = float("-inf")
         beta = float("inf")
         max_value = float("-inf")
         best_move = [] 
 
-        for move in state.availableMoves():
-            value = self.abSearch(state, move, self.last_move,alpha, beta, h, True)
+        for move in self.state.availableMoves():
+            value = self.abSearch(move, self.last_move,alpha, beta, h, True)
             if value > max_value:
                 max_value = value
                 alpha = value
@@ -41,31 +42,31 @@ class Agent(object):
 
         return max_value, best_move
     
-    def abSearch(self, state, move, prev_next_big, alpha, beta, h, maximize):
+    def abSearch(self, move, prev_next_big, alpha, beta, h, maximize):
         # Check if time has ran out
         if time.time() - self.start > self.playclock:
             raise Exception()
 
         # Check if we have reached a terminal state
-        if state.won == self.symbol:
+        if self.state.won == self.symbol:
             return WIN
-        elif state.won != "D" and state.won != self.symbol:
+        elif self.state.won != "D" and self.state.won != self.symbol:
             return LOSS
-        elif state.won:
+        elif self.state.won:
             return DRAW
 
         # Check if max depth is reached
         if h == 0:
             # took in laststate and 
-            return self.evaluateState(state)
+            return self.evaluateState(self.state)
 
         # Apply alpha beta pruning
         best_value = float("-inf") if maximize else float("inf")
-        for move in state.availableMoves():
+        for move in self.state.availableMoves():
             # Get next state, state changes when calling makeMove
-            prev_next_big = state.next_big
-            state.makeMove(move)
-            v = self.abSearch(state, move, state.next_big, alpha, beta, h - 1, not maximize)
+            prev_next_big = self.state.next_big
+            self.state.makeMove(move)
+            v = self.abSearch(move, self.state.next_big, alpha, beta, h - 1, not maximize)
             if maximize:
                 if v > best_value:
                     best_value = v
